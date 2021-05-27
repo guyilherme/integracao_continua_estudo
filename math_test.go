e3f416aba792c5a6892ad313d274c83a71d00269
Guilherme Macêdo da Cruz <eu@guilhermecruz.dev>
2021-05-26 21:43:21 -0300

    fix: :bug: Corrigindo nome da função

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := Soma(10, 10)
+	total := soma(10, 10)
 
 	if total != 20 {
 		t.Errorf("Resultado soma incorreto. Era esperado %d e veio %d", 20, total)
